43e492f661d063d38faaad44013620368796da3f
Alex Ball <me@alexjball.com>
2020-08-16 18:54:36 -0400

    TDD tictactoe

--- packages/tic-tac-toe/src/__tests__/index.ts
+++ packages/tic-tac-toe/src/__tests__/index.ts
@@ -1,10 +1,64 @@
-import { TicTacToe } from '../';
+import { TicTacToe, IllegalMoveError } from '../';
 
-const player1Name = 'player 1';
-const player2Name = 'player 2';
+const player1 = 'player 1';
+const player2 = 'player 2';
 
 it('reflects players', () => {
-  const game = new TicTacToe(player1Name, player2Name);
-  expect(game.player1).toEqual(player1Name);
-  expect(game.player2).toEqual(player2Name);
+  const game = new TicTacToe({ player1, player2, boardSize: 3 });
+  expect(game.player1).toEqual(player1);
+  expect(game.player2).toEqual(player2);
+});
+
+it('accepts valid moves', () => {
+  const game = new TicTacToe({ player1, player2, boardSize: 3 });
+
+  game.choose(0, 0);
+
+  expect(game.board[0][0]).toEqual(player1);
+});
+
+it('switches players', () => {
+  const game = new TicTacToe({ player1, player2, boardSize: 3 });
+
+  game.choose(0, 0);
+  game.choose(1, 0);
+
+  expect(game.board[1][0]).toEqual(player2);
+});
+
+it('rejects invalid moves', () => {
+  const game = new TicTacToe({ player1, player2, boardSize: 3 });
+
+  expect(() => game.choose(-1, 4)).toThrowError(IllegalMoveError);
+});
+
+it('rejects occupied spots', () => {
+  const game = new TicTacToe({ player1, player2, boardSize: 3 });
+
+  game.choose(0, 0);
+  expect(() => game.choose(0, 0)).toThrowError(IllegalMoveError);
+});
+
+it('reports winner', () => {
+  const game = new TicTacToe({ player1, player2, boardSize: 3 });
+
+  game.choose(0, 0);
+  game.choose(0, 1);
+  game.choose(1, 1);
+  game.choose(0, 2);
+  game.choose(2, 2);
+
+  expect(game.winner).toEqual(player1);
+});
+
+it('rejects moves after winner', () => {
+  const game = new TicTacToe({ player1, player2, boardSize: 3 });
+
+  game.choose(0, 0);
+  game.choose(0, 1);
+  game.choose(1, 1);
+  game.choose(0, 2);
+  game.choose(2, 2);
+
+  expect(() => game.choose(1, 0)).toThrowError(IllegalMoveError);
 });
--- packages/tic-tac-toe/src/index.ts
+++ packages/tic-tac-toe/src/index.ts
@@ -1,9 +1,81 @@
+interface TicTacToeCtor {
+  player1: string;
+  player2: string;
+  boardSize: number;
+}
 export class TicTacToe {
   readonly player1: string;
   readonly player2: string;
 
-  constructor(player1: string, player2: string) {
+  board: Array<Array<string | null>>;
+  boardSize: number;
+  waitingOn: string;
+  winner: string | null = null;
+
+  constructor({ player1, player2, boardSize }: TicTacToeCtor) {
     this.player1 = player1;
     this.player2 = player2;
+    this.board = [...Array(boardSize)].map(() => Array(boardSize).fill(null));
+    this.boardSize = boardSize;
+    this.waitingOn = player1;
+  }
+
+  choose(row: number, col: number): void {
+    if (this.winner) {
+      throw new IllegalMoveError('Game is complete');
+    }
+    this.validateChoice(row, col);
+    if (this.board[row][col]) {
+      throw new IllegalMoveError('Space already chosen');
+    }
+
+    this.board[row][col] = this.waitingOn;
+    this.waitingOn =
+      this.waitingOn == this.player1 ? this.player2 : this.player1;
+    this.updateWinner();
+  }
+
+  validateChoice(row: number, col: number): void {
+    [row, col].forEach(i => {
+      if (Math.round(i) != i || i < 0 || i >= this.boardSize) {
+        throw new IllegalMoveError('invalid row and col');
+      }
+    });
+  }
+
+  updateWinner(): void {
+    this.visit(slice => {
+      if (slice.every(v => v === this.player1)) {
+        this.winner = this.player1;
+      } else if (slice.every(v => v === this.player2)) {
+        this.winner = this.player2;
+      }
+    });
+  }
+
+  visit(cb: (slice: Array<string | null>) => void): void {
+    let slice: Array<string | null> = [];
+    const slice2: Array<string | null> = [];
+    this.board.forEach(row => cb(row));
+    for (let col = 0; col < this.boardSize; col++) {
+      slice = [];
+      for (let row = 0; row < this.boardSize; row++) {
+        slice.push(this.board[row][col]);
+      }
+      cb(slice);
+    }
+    slice = [];
+    for (let i = 0; i < this.boardSize; i++) {
+      slice.push(this.board[i][i]);
+      slice2.push(this.board[i][this.boardSize - 1 - i]);
+    }
+    cb(slice);
+  }
+}
+
+export class IllegalMoveError extends Error {
+  constructor(message: string) {
+    super(message);
+    Object.setPrototypeOf(this, new.target.prototype);
   }
 }
